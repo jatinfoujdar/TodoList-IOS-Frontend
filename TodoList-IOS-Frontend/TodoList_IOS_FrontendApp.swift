8ec1ea88a129204bfa3cc6a9d343ff03febeb3f5
Jatin Foujdar <jatin321001@gmail.com>
2024-11-16 22:06:26 +0530

    Initial Commit

---
+++ TodoList-IOS-Frontend/TodoList_IOS_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_IOS_FrontendApp.swift
+//  TodoList-IOS-Frontend
+//
+//  Created by jatin foujdar on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_IOS_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoList-IOS-Frontend/View/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  TodoList-IOS-Frontend
+//
+//  Created by jatin foujdar on 16/11/24.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}
